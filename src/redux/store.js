c9e09ad9a45f312fa52a41cf7929cf603c353ac0
amdat99 <aamirdat99@gmail.com>
2021-02-13 00:36:14 +0000

    adding heroku fetch reqh

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware} from 'redux'
 import { persistStore } from 'redux-persist'
-import logger from 'redux-logger'
+// import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
 
 // import { fetchItemsPending } from './shop/shop.sagas'
@@ -11,11 +11,11 @@ import rootSaga from './root-saga';
 const sagaMiddleware = createSagaMiddleware();
 
 
-const middlewares = [sagaMiddleware];
+const middlewares = [ sagaMiddleware];
 
-if(process.env.NODE_ENV === 'development'){
-    middlewares.push(logger)
-}
+// if(process.env.NODE_ENV === 'development'){
+//     middlewares.push(logger)
+// }
 
  export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
